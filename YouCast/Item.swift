4b991e0bd44a47cb15c9b0f87b99a18d83d51f59
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-08-22 20:51:23 +0100

    Initial Commit

---
+++ YouCast/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  YouCast
+//
+//  Created by Ayodeji Osasona on 22/08/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
