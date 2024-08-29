e65dac006140f8f1869b8d5eea82cca87c6e4d00
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-08-29 21:18:28 +0100

    Added naive queue implementation

--- YouCast/Lib/Queue.swift
+++ YouCast/Lib/Queue.swift
@@ -6,4 +6,39 @@
 //
 import Foundation
 
-class Queue: ObservableObject {}
+class Queue<T>: ObservableObject {
+    private let dispatchQueue: DispatchQueue = .global()
+    private var items: [T] = []
+    private var execute: (T) -> Void = { _ in }
+    private var isRunning: Bool = false
+    
+    var isEmpty: Bool { items.isEmpty }
+    var count: Int { items.count }
+
+    func enqueue(_ item: T) {
+        items.append(item)
+        
+        if !isRunning { start() }
+    }
+    
+    func dequeue() -> T? {
+        items.removeFirst()
+    }
+    
+    func execute(_ execute: @escaping (T) -> Void) {
+        self.execute = execute
+    }
+    
+    func start() {
+        if isRunning { return }
+        isRunning = true
+        
+        dispatchQueue.async {
+            while let item = self.dequeue() {
+                self.execute(item)
+            }
+            
+            if self.isEmpty { self.isRunning = false }
+        }
+    }
+}
