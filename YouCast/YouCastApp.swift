5082824e74f9ac9a24ca7e9499a9e811111959ba
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-09-07 16:44:01 +0100

    Updated root to use database class

--- YouCast/YouCastApp.swift
+++ YouCast/YouCastApp.swift
@@ -10,24 +10,34 @@ import SwiftUI
 
 @main
 struct YouCastApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Entry.self,
-            Collection.self
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
+    var database: Database = {
+        guard let database = Database.shared else { fatalError() }
+        return database
     }()
+
+    var entryQueue: Queue<Entry> {
+        let q = Queue<Entry>()
+
+        // TODO: pass in a proper execution function
+        q.execute { entry in
+            print("Executing: \(entry)")
+        }
+
+        return q
+    }
 
     var body: some Scene {
         WindowGroup {
             Root()
+                .environmentObject(database)
+                .environmentObject(entryQueue)
+                .task {
+                    let unprocessedEntries = Entry.loadUnprocessedItems(db: database)
+                    print(unprocessedEntries)
+
+                    entryQueue.start()
+                }
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(database.getModelContainer())
     }
 }
