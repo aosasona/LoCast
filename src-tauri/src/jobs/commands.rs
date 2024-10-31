5c8b782136e6eef085e0b9e1812f1960b71ee4bb
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-10-31 07:04:22 +0000

    WIP: import YT video command

---
+++ src-tauri/src/jobs/commands.rs
@@ -0,0 +1,3 @@
+async fn import_video() -> anyhow::Result<()> {
+    Ok(())
+}
--- src-tauri/src/jobs/mod.rs
+++ src-tauri/src/jobs/mod.rs
@@ -1,5 +1,7 @@
+mod commands;
 mod manager;
 mod types;
 
+pub use commands::*;
 pub use manager::*;
 pub use types::*;
