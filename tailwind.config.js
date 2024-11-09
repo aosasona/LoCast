dc386bf42d09993d2fddc9ced91c8d622836cb1f
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-09 00:03:27 +0000

    Added new spacing sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
 	darkMode: "class",
 	theme: {
 		extend: {
+			spacing: {
+				"safe-top": "env(safe-area-inset-top)",
+				"safe-bottom": "env(safe-area-inset-bottom)",
+				"safe-left": "env(safe-area-inset-left)",
+				"safe-right": "env(safe-area-inset-right)",
+			},
 			colors: {
 				dark: "hsl(var(--color-dark))",
 				body: "hsl(var(--color-body))",
